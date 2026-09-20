535a4e851bf57697be13be02825b1f0f9299b847
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-09-21 07:12:05 +0900

    brew: add: zen browser

--- home-manager/hotkey/app.nix
+++ home-manager/hotkey/app.nix
@@ -2,7 +2,7 @@ _: {
   services.skhd = {
     enable = true;
     config = ''
-      ctrl + alt - b : open -a "Brave Browser.app"
+      ctrl + alt - b : open -a "Zen.app"
       ctrl + alt - t : open -a "WezTerm.app"
       ctrl + alt - m : open -a "Amazon Music.app"
     '';
--- nix-darwin/homebrew.nix
+++ nix-darwin/homebrew.nix
@@ -24,6 +24,7 @@
       "obsidian"
       "slack"
       "steam"
+      "zen"
     ];
   };
 }
